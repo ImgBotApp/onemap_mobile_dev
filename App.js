157a3142319a50abce681b899a8b608e9d8a17a3
RoyalDeveloper2015 <top.webdeveloper2@gmail.com>
2017-12-23 08:35:19 +1000

    init project

--- App.js
+++ App.js
@@ -5,53 +5,43 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import userReducers from '@reducers/userLoginReducers'
+import MainRoute from '@routes'
+
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { connect } from 'react-redux'
+
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
+import { addNavigationHelpers } from 'react-navigation'
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjb30vkvv434c0146sjjn4d4w' })
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+let store = createStore(combineReducers({userReducers}))
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <ApolloProvider client={client} store={store}>
+        <NaviState />
+      </ApolloProvider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const mapStateToProps = (state) => state
+// create a component
+class MyClass extends Component {
+  render() {
+    return (
+      <MainRoute />
+    );
+  }
+}
+
+const NaviState = connect(mapStateToProps)(MyClass)
