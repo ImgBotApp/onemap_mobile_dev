056f74744a77c38065fe3bfe0e90be971d51f235
redrose <business.mobile218@gmail.com>
2018-01-22 06:49:14 +0200

    keyword in place profile page updated

--- src/containers/FeedModule/PlaceProfile/index.js
+++ src/containers/FeedModule/PlaceProfile/index.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux'
 import { graphql } from 'react-apollo'
-import { CREATE_KEYWORD, UPDATE_KEYWORD } from "@graphql/keywords";
+import { CREATE_KEYWORD, DELETE_KEYWORD } from "@graphql/keywords";
 import { ADD_COLLECTION_TO_PLACE, REMOVE_COLLECTION_FROM_PLACE } from "@graphql/places";
+import { CREATE_STORY, UPDATE_STORY } from "@graphql/users";
 import page from './page'
 
 function mapStateToProps(state) {
@@ -20,6 +21,8 @@ function mapDispatchToProps(dispatch) {
 let container = graphql(ADD_COLLECTION_TO_PLACE, { name: 'addCollectionToPlace' })(page);
 container = graphql(REMOVE_COLLECTION_FROM_PLACE, { name: 'removeCollectionFromPlace' })(container);
 container = graphql(CREATE_KEYWORD, { name: 'addKeyword' })(container);
-container = graphql(UPDATE_KEYWORD, { name: 'updateKeyword' })(container);
+container = graphql(DELETE_KEYWORD, { name: 'deleteKeyword' })(container);
+container = graphql(CREATE_STORY, { name: 'createStory' })(container);
+container = graphql(UPDATE_STORY, { name: 'updateStory' })(container);
 
 export default connect(mapStateToProps, mapDispatchToProps)(container)
