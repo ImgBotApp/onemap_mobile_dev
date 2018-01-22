acd0e8f0283c3636dc00ef595312887c01be792e
redrose <business.mobile218@gmail.com>
2018-01-22 11:24:27 +0200

    react-native-cloudinary added

--- android/app/src/main/java/com/onemapfinal/MainApplication.java
+++ android/app/src/main/java/com/onemapfinal/MainApplication.java
@@ -3,6 +3,7 @@ package com.onemapfinal;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rncloudinary.RNCloudinaryPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.github.yamill.orientation.OrientationPackage;
@@ -44,6 +45,7 @@ public class MainApplication extends NavigationApplication implements ReactAppli
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new RNCloudinaryPackage(),
             new PickerPackage(),
             new ReactVideoPackage(),
             new OrientationPackage(),
